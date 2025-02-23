3d2be2db2f000cec30a9c74f1508ef301bf51fd6
Quoc.89 <134296628+quoc10973@users.noreply.github.com>
2025-02-23 15:57:46 +0700

    enable cors

--- Nest/src/main.ts
+++ Nest/src/main.ts
@@ -3,7 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173', // react app
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
   await app.listen(process.env.PORT ?? 3000);
+
 }
 
 bootstrap();
